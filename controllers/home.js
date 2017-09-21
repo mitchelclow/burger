ceab480a4ae2acad1041043d8b95a514f65012ef
mitchelclow <mitchelclow@gmail.com>
2017-09-20 20:04:03 -0600

    adding in mysql database

--- controllers/home.js
+++ controllers/home.js
@@ -1,54 +1,94 @@
+// var express = require('express')
+// var app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
+
+// // GET method route
+// app.get('/', function (req, res) {
+//   res.send('GET request to the homepage')
+// })
+
+// // POST method route
+// app.post('/', function (req, res) {
+//   res.send('POST request to the homepage')
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const express = require('express');
 // var router = express.Router();
 
-// router.get("/", function(req, res){
-//   res.render("home", {title:"yuck"})
-// })
+// // router.get("/", function(req, res){
+// //   res.render("home", {title:"yuck"})
+// // })
 // module.exports=router
 
 
-var express = require("express");
+// var express = require("express");
 
-var router = express.Router();
+// var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// // Import the model (cat.js) to use its database functions.
 
-// var cat = require("../models/cat.js");
+// // var cat = require("../models/cat.js");
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-    res.render("index");
-});
-
-// router.post("/", function(req, res) {
-//   cat.create([
-//     "name", "sleepy"
-//   ], [
-//     req.body.name, req.body.sleepy
-//   ], function() {
-//     res.redirect("/");
-//   });
+// // Create all our routes and set up logic within those routes where required.
+// router.get("/", function(req, res) {
+//     res.render("index");
 // });
 
-// router.put("/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+// // router.post("/", function(req, res) {
+// //   cat.create([
+// //     "name", "sleepy"
+// //   ], [
+// //     req.body.name, req.body.sleepy
+// //   ], function() {
+// //     res.redirect("/");
+// //   });
+// // });
 
-//   console.log("condition", condition);
+// // router.put("/:id", function(req, res) {
+// //   var condition = "id = " + req.params.id;
 
-//   cat.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function() {
-//     res.redirect("/");
-//   });
-// });
+// //   console.log("condition", condition);
 
-// router.delete("/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+// //   cat.update({
+// //     sleepy: req.body.sleepy
+// //   }, condition, function() {
+// //     res.redirect("/");
+// //   });
+// // });
 
-//   cat.delete(condition, function() {
-//     res.redirect("/");
-//   });
-// });
+// // router.delete("/:id", function(req, res) {
+// //   var condition = "id = " + req.params.id;
 
-// Export routes for server.js to use.
-module.exports = router;
+// //   cat.delete(condition, function() {
+// //     res.redirect("/");
+// //   });
+// // });
+
+// // Export routes for server.js to use.
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 // Requiring npm packages
 var express = require('express');
+
 var bodyParser = require('body-parser');
+
 var exphbs = require('express-handlebars');
+
 var inquirer = require('inquirer');
+
 var path = require('path');
 
+var mysql = require('mysql')
+
 var homeRouter = require('./controllers/home.js');
+
 var app = express();
 var port = process.env.PORT || 3000
 app.use(express.static("public"));
@@ -20,4 +27,24 @@ app.listen(port, function(){
   console.log("server listening on " + port);
 })
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'waterbottle',
+  database : 'burgers_db'
+});
 
+// Host: codeflink.net
+// Username: mitchclow
+// Password: bPEoSo5ObbY7
+// Database: mitchclow
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
